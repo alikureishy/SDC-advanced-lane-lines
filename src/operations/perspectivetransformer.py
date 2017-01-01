e6a46534fafcaabfa3c843232a1e621202179e6f
Safdar Kureishy <safdar.kureishy@gmail.com>
2017-01-01 13:59:06 -0800

    Perspective transformer now uses ratios instead of specific points

--- src/operations/perspectivetransformer.py
+++ src/operations/perspectivetransformer.py
@@ -11,8 +11,8 @@ from utils.utilities import plotboundary
 
 class PerspectiveTransformer(Operation):
     # Config
-    SrcPoints = 'SrcPoints'
-    DestPoints = 'DestPoints'
+    DefaultPerspectiveRatios = 'PerspectiveRatios'
+    TransformRatios = 'TransformRatios'
     
     # Outputs:
     WarpedColor = "WarpedColor"
@@ -20,37 +20,34 @@ class PerspectiveTransformer(Operation):
     def __init__(self, params):
         Operation.__init__(self, params)
         
-        # Later it might be good to figure out the source/destination points automatically:
         self.__M__ = None
         self.__Minv__ = None
-        self.__srcpoints__ = None
-        self.__destpoints__ = None
+        self.__default_pers_ratios__ = params[self.DefaultPerspectiveRatios]
+        self.__transform_ratios__ = params[self.TransformRatios]
+        self.__perspective_points__ = None
+        self.__transform_points__ = None
         
-        # Attempt to read static points from config:
-        if ((self.SrcPoints in params) & (self.DestPoints in params)):
-            self.__srcpoints__, self.__destpoints__ = params[self.SrcPoints], params[self.DestPoints]
-            self.__M__, self.__Minv__ = self.gettransformations(params[self.SrcPoints], params[self.DestPoints])
-        else:
-            raise "Source and/or destination points not provided for perspective transformation"
-
     def __processupstream__(self, original, latest, data, frame):
         x_dim = latest.shape[1]
         y_dim = latest.shape[0]
 
-        # Obtain the perspective and transform points:
-        
+        # Calculate and cache the transform and perspective points since these will never change:
+        if self.__transform_points__ is None:
+            self.__transform_points__ = [[int(x_ratio*x_dim), int(y_ratio*y_dim)] for [x_ratio,y_ratio] in self.__transform_ratios__]
+            self.__perspective_points__ = [[int(x_ratio*x_dim), int(y_ratio*y_dim)] for [x_ratio,y_ratio] in self.__default_pers_ratios__]
+            self.__M__, self.__Minv__ = self.gettransformations(self.__perspective_points__, self.__transform_points__)
 
         # Plot the source points, for the benefit of the viewer
         if self.isplotting():
             # Show perspective regions:
             orig = np.copy(self.getdata(data, self.Upstream, ColorSpacer))
-            plotboundary(orig, self.__srcpoints__, (127, 255, 212))
+            plotboundary(orig, self.__perspective_points__, (127, 255, 212))
             self.__plot__(frame, orig, None, "Warp Region (Source)", None)
 
             # Show warped original:
             orig = np.copy(self.getdata(data, self.Upstream, ColorSpacer))
             warped_orig = cv2.warpPerspective(orig, self.__M__, (x_dim, y_dim), flags=cv2.INTER_LINEAR)
-            plotboundary(warped_orig, self.__destpoints__, (255, 192, 203))
+            plotboundary(warped_orig, self.__transform_points__, (255, 192, 203))
             self.__plot__(frame, warped_orig, None, "Warped (Original)", None)
             self.setdata(data, self.WarpedColor, warped_orig)
 
