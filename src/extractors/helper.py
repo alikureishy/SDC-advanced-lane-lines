0a2c94c9f638fea97706b2101f56a55ae5488f4c
Safdar Kureishy <safdar.kureishy@gmail.com>
2017-01-17 08:10:28 +0300

    Polished the window search grid and integrated classifier trainer into lanemapper.json

--- src/extractors/helper.py
+++ src/extractors/helper.py
@@ -16,10 +16,9 @@ class Types(object):
         Space = 'Space'
         NumBins = 'NumBins'
         BinRange = 'BinRange'
-        Channel = 'Channel'
     class HOGExtractor(object):
         Orientations = 'Orientations'
-        Channel = 'Channel'
+        HogChannel = 'HogChannel'
         PixelsPerCell = 'PixelsPerCell'
         CellsPerBlock = 'CellsPerBlock'
         Size = 'Size'
@@ -32,13 +31,13 @@ def buildextractor(extractorsequence):
         if Types.ColorHistogram.__name__ in config:
             cfg = config[Types.ColorHistogram.__name__]
             extractor = ColorHistogram(color_space=cfg[Types.ColorHistogram.Space], nbins=cfg[Types.ColorHistogram.NumBins], 
-                bins_range=cfg[Types.ColorHistogram.BinRange], channel=cfg[Types.ColorHistogram.Channel])
+                bins_range=cfg[Types.ColorHistogram.BinRange])
         elif Types.SpatialBinning.__name__ in config:
             cfg = config[Types.SpatialBinning.__name__]
             extractor = SpatialBinner(color_space=cfg[Types.SpatialBinning.Space], size=cfg[Types.SpatialBinning.Size])
         elif Types.HOGExtractor.__name__ in config:
             cfg = config[Types.HOGExtractor.__name__]
-            extractor = HogExtractor(orientations=cfg[Types.HOGExtractor.Orientations], channel=cfg[Types.HOGExtractor.Channel], 
+            extractor = HogExtractor(orientations=cfg[Types.HOGExtractor.Orientations], hog_channel=cfg[Types.HOGExtractor.HogChannel], 
                 size=cfg[Types.SpatialBinning.Size], 
                 pixels_per_cell=cfg[Types.HOGExtractor.PixelsPerCell], 
                 cells_per_block=cfg[Types.HOGExtractor.CellsPerBlock])
