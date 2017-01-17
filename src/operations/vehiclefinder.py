722a6ef3b53d0deaa52a777e6bf582875f59ef57
Safdar Kureishy <safdar.kureishy@gmail.com>
2017-01-17 08:10:28 +0300

    Polished the window search grid and integrated classifier trainer into lanemapper.json

--- src/operations/vehiclefinder.py
+++ src/operations/vehiclefinder.py
@@ -14,72 +14,33 @@ from statistics import mean
 import numpy as np
 from utils.utilities import getperspectivepoints
 import cv2
+from extractors.helper import buildextractor
 
 class VehicleFinder(Operation):
     ClassifierFile = 'ClassifierFile'
     FeatureExtractors = 'FeatureExtractors'
-    class Plottable(object):
-        ToPlot = 'ToPlot'
-    class SlidingWindow(Plottable):
+    SlidingWindow = 'SlidingWindow'
+    class SlidingWindow(object):
         DepthRangeRatio = 'DepthRangeRatio'
         DefaultHeadingRatios = 'DefaultHeadingRatios'
         CenterShiftRatio = 'CenterShiftRatio'
         SizeVariations = 'SizeVariations'
         WindowRangeRatio = 'WindowRangeRatio'
         StepRatio = 'StepRatio'
-    class SpatialBinning(Plottable):
-        Space = 'Space'
-        Size = 'Size'
-    class ColorHistogram(Plottable):
-        Space = 'Space'
-        NumBins = 'NumBins'
-        BinRange = 'BinRange'
-    class HOGExtractor(Plottable):
-        Orientations = 'Orientations'
-        HogChannel = 'HogChannel'
-        PixelsPerCell = 'PixelsPerCell'
-        CellsPerBlock = 'CellsPerBlock'
-        Size = 'Size'
 
     # Constants:
-    HitWindowColor = [152, 251, 152]
-    MissWindowColor = [255,160,122]
+    HitWindowColor = [255, 0, 0]
+    MissWindowColor = [152, 0, 0]
+
 
     def __init__(self, params):
         Operation.__init__(self, params)
-
-        # Classifier
         self.__classifier__ = joblib.load(params[self.ClassifierFile])
-
-        # Sliding Window:
         self.__windows__ = None
         
         # Feature Extractors
         extractorsequence = params[self.FeatureExtractors]
-        extractors = []
-        for config in extractorsequence:
-            assert len(config.keys())==1, "Invalid: {}".format(config)
-            extractor = None
-            if self.ColorHistogram.__name__ in config:
-                cfg = config[self.ColorHistogram.__name__]
-                extractor = ColorHistogram(color_space=cfg[self.ColorHistogram.Space], 
-                                           nbins=cfg[self.ColorHistogram.NumBins], 
-                                           bins_range=cfg[self.ColorHistogram.BinRange])
-            elif self.SpatialBinning.__name__ in config:
-                cfg = config[self.SpatialBinning.__name__]
-                extractor = SpatialBinner(color_space=cfg[self.SpatialBinning.Space],
-                                          size=cfg[self.SpatialBinning.Size])
-            elif self.HOGExtractor.__name__ in config:
-                cfg = config[self.HOGExtractor.__name__]
-                extractor = HogExtractor(orientations=cfg[self.HOGExtractor.Orientations],
-                                          hog_channel=cfg[self.HOGExtractor.HogChannel],
-                                          size=cfg[self.SpatialBinning.Size],
-                                          pixels_per_cell=cfg[self.HOGExtractor.PixelsPerCell],
-                                          cells_per_block=cfg[self.HOGExtractor.CellsPerBlock])
-            else:
-                raise ("Unrecognized extractor provided")
-            extractors.append(extractor)
-        self.__feature_extractor__ = FeatureCombiner(tuple(extractors))
+        self.__feature_extractor__ = buildextractor(extractorsequence)
 
     def __processupstream__(self, original, latest, data, frame):
         x_dim, y_dim, xy_avg = latest.shape[1], latest.shape[0], int(mean(latest.shape[0:2]))
@@ -87,14 +48,6 @@ class VehicleFinder(Operation):
         if self.__windows__ is None:
             self.__windows__ = self.generatewindows(slidingwindowconfig, x_dim, y_dim, xy_avg)
 
-        # Illustrate boxes:
-        if self.isplotting():
-            image = np.copy(latest)
-            for scan in self.__windows__:
-                for (x1,x2,y1,y2) in scan:
-                    cv2.rectangle(image, (x1,y1), (x2,y2), self.MissWindowColor, 5)
-            self.__plot__(frame, image, None, "Vehicle Search Windows", None)
-        
         # Perform search:
         image = np.copy(latest)
         detected_vehicles = []
@@ -107,14 +60,26 @@ class VehicleFinder(Operation):
                     detected_vehicles.append((x1,x2,y1,y2))
 
         if self.isplotting():
-            image = np.copy(latest)
+            image_all = np.zeros_like(latest)
+            for scan in self.__windows__:
+                for (x1,x2,y1,y2) in scan:
+                    cv2.rectangle(image_all, (x1,y1), (x2,y2), self.MissWindowColor, 3)
+            image_hits = np.copy(latest)
             for (x1,x2,y1,y2) in detected_vehicles:
-                cv2.rectangle(image, (x1,y1), (x2,y2), self.HitWindowColor, 5)
-            self.__plot__(frame, image, None, "Vehicle Search Hits", None)
+                cv2.rectangle(image_hits, (x1,y1), (x2,y2), self.HitWindowColor, 4)
+            todraw = cv2.addWeighted(image_hits, 1, image_all, 0.4, 0)
+            self.__plot__(frame, todraw, None, "Vehicle Search & Hits", None)
             
         return latest
 
     def getboxcorners(self, center, size, xrange, yrange):
+        if center[0] <= xrange[0] or center[0] >= xrange[1]:
+#             print ("Warning: Attempt to generate windows around overflowing center: {} with size: {}".format(center, size))
+            return None
+        if center[1] <= yrange[0] or center[1] >= yrange[1]:
+#             print ("Warning: Attempt to generate windows around overflowing center: {} with size: {}".format(center, size))
+            return None
+        
         s = int(size/2)
         x = center[0]
         y = center[1]
@@ -123,53 +88,56 @@ class VehicleFinder(Operation):
         rightx = x+s
         topy = y-s
         bottomy = y+s
-        if leftx < xrange[0] or rightx > xrange[1] or topy < yrange[0] or bottomy > yrange[1]:
-            return None
-        
-#         topleft = (leftx, topy)
-#         topright = (rightx, topy)
-#         bottomleft = (leftx, bottomy)
-#         bottomright = (rightx, bottomy)
-#         return (topleft, topright, bottomleft, bottomright)
+        if leftx < xrange[0]:
+            leftx = xrange[0]
+        if rightx > xrange[1]:
+            rightx = xrange[1]
+        if topy < yrange[0]:
+            topy = yrange[0]
+        if bottomy > yrange[1]:
+            bottomy = yrange[1]
+            
+        assert not (leftx == rightx) or not (topy == bottomy), "Zero sized window found with center: {}, size: {}".format(center, size)
+        return (leftx, rightx, topy, bottomy)
     
-        return (leftx, rightx, topy, bottomy) 
     def generatewindows(self, cfg, x_dim, y_dim, xy_avg):
         shift = cfg[self.SlidingWindow.CenterShiftRatio] * x_dim
-        # Depth range:
         depth_range_ratio = sorted(cfg[self.SlidingWindow.DepthRangeRatio], reverse=True)
-        depth_range = [int(y_dim * r) for r in depth_range_ratio]
-        horizon = min(depth_range)
-        # Calculate perspective widths for depth range:
-        default_heading = cfg[self.SlidingWindow.DefaultHeadingRatios]
-        points = getperspectivepoints(x_dim, y_dim, depth_range_ratio, default_heading)
-        perspective_widths = sorted([(points[1][0] - points[0][0]), (points[3][0] - points[2][0])])
-        # Magnification of scan widths and windows across depth:
-        magnification = perspective_widths[1] / perspective_widths[0]
-        # Calculate horizon scan range and window size:
-        scan_to_perspective_ratio = x_dim / perspective_widths[1]
-        horizon_scan_width = int((scan_to_perspective_ratio * perspective_widths[0]) / 2)
+        horizon = min([int(y_dim * r) for r in depth_range_ratio])
+        print ("Horizon is at depth: {}".format(horizon))
         window_range_ratio = cfg[self.SlidingWindow.WindowRangeRatio]
         window_range = [int(xy_avg * r) for r in window_range_ratio]
+        print ("Window range: {}".format(window_range))
         size_variations = cfg[self.SlidingWindow.SizeVariations]
         grow_rate = int(np.absolute(window_range[1]-window_range[0])/(size_variations))
+        print ("Window grow rate (each side): {}".format(grow_rate))
         slide_ratio = cfg[self.SlidingWindow.StepRatio]
-        # Calculate all the windows:
         center = (int((x_dim / 2) + shift), horizon)
+        print ("Center of vision: {}".format(center))
+        
         windows = []
-        # Each slice:
         for i in range(size_variations):
+            print ("Scan # {}".format(i))
             windows.append([])
-            scanwidth = horizon_scan_width + (i * grow_rate * magnification)
+            scanwidth = int(x_dim/2)
+            print ("\tScan width: {}".format(scanwidth))
             boxwidth = window_range[0] + (i * grow_rate)
+            print ("\tBox width: {}".format(boxwidth))
             center_box = self.getboxcorners(center, boxwidth, [0, x_dim], [0, y_dim])
             if center_box is None:
+                print ("\tCenter box (OUTSIDE BOUNDS): {}".format(center_box))
                 continue
+            print ("\tCenter box: {}".format(center_box))
             windows[i].append(center_box)
             shifts_per_box = int(1 / slide_ratio)
             boxshift = int(boxwidth * slide_ratio)
+            print ("\t\tBox shift: {}".format(boxshift))
             numboxes = int(scanwidth / boxwidth) # Boxes each side of the center box
+            print ("\t\tNum boxes: {}".format(numboxes))
             # Each box on left + right sides of center:
+            print ("\t\t\tShifts Per Box: {}".format(shifts_per_box))
             for j in range(1, numboxes + 1):
+                print ("\t\t\tShifted Boxes # ({})".format('~'*j))
                 for k in range(0, shifts_per_box):
                     leftcenter = (center[0] - (j * boxwidth) - (k * boxshift), center[1])
                     left_box = self.getboxcorners(leftcenter, boxwidth, [0, x_dim], [0, y_dim])
@@ -179,6 +147,7 @@ class VehicleFinder(Operation):
                         continue
                     windows[i].append(left_box)
                     windows[i].append(right_box)
-        
+                    print ("\t\t\t\tShift # {}: <--{} {} {}-->".format(k, left_box, '~'*(k+1), right_box))
+            print ("\tTotal boxes in scan: {}".format(len(windows[i])))
         return windows
 
