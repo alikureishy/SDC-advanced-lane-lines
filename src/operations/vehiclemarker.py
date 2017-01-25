acdeaa5dfcee3f7c35c0d1ec2289ad8c36c072f9
Safdar Kureishy <safdar.kureishy@gmail.com>
2017-01-25 17:59:41 +0300

    Got initial cut of vehicle detection done. Now need to prune settings to improve accuracy.

--- src/operations/vehiclemarker.py
+++ src/operations/vehiclemarker.py
@@ -6,11 +6,12 @@ Created on Jan 15, 2017
 
 from operations.baseoperation import Operation
 from operations.vehicletracker import VehicleTracker
+from utils.plotter import Image
 import cv2
 
 class VehicleMarker(Operation):
     # Configuration:
-    CertaintyThreshold = 'CertainThreshold'
+    ConfidenceThreshold = 'ConfidenceThreshold'
     AgeThreshold = 'AgeThreshold'
     
 #     CurrentBoundingBoxColor = 'CurrentBoundingBoxColor'
@@ -20,14 +21,17 @@ class VehicleMarker(Operation):
     
     def __init__(self, params):
         Operation.__init__(self, params)
-        self.__certainty_threshold__ = params[self.CertaintyThreshold]
+        self.__confidence_threshold__ = params[self.ConfidenceThreshold]
         self.__age_threshold__ = params[self.AgeThreshold]
 #         self.__bounding_box_color__ = params[self.CurrentBoundingBoxColor]
 
     def __processdownstream__(self, original, latest, data, frame):
         cartracker = self.getdata(data, VehicleTracker.CarTracker, VehicleTracker)
-        cars = cartracker.get_cars(certainty_threshold=self.__certainty_threshold__, age_threshold=self.__age_threshold__)
+        cars = cartracker.get_cars(confidence_threshold=self.__confidence_threshold__, age_threshold=self.__age_threshold__)
         for car in cars:
-            (x1,x2,y1,y2) = car.get_box()
+            (x1,x2,y1,y2) = car.boundary()
             cv2.rectangle(latest, (x1,y1), (x2,y2), self.StrongWindowColor, 2)
+            
+        if self.isplotting():
+            self.__plot__(frame, Image("Marked Vehicles", latest, None))
         return latest
